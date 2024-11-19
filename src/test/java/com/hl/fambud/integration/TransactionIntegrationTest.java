751a8f966c7d07e2981bafff3d3d28cbd7fa83a1
hugolindin <hugolindin@gmail.com>
2024-11-19 22:48:58 +1100

    transaction file import integration test disabled

--- src/test/java/com/hl/fambud/integration/TransactionIntegrationTest.java
+++ src/test/java/com/hl/fambud/integration/TransactionIntegrationTest.java
@@ -3,23 +3,17 @@ package com.hl.fambud.integration;
 import com.hl.fambud.model.TransactionType;
 import com.hl.fambud.repository.TransactionRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import reactor.test.StepVerifier;
-
-import java.io.File;
-import java.io.FileInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -36,13 +30,15 @@ public class TransactionIntegrationTest {
     private TransactionRepository transactionRepository;
 
     @Test
+    @Disabled
     public void importTransactionsCsv() throws Exception {
         MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
-        bodyBuilder.part("file", new ClassPathResource("standard-format-transactions-10.csv"));
+        bodyBuilder.part("file", new ClassPathResource("standard-format-transactions-10.csv"))
+            .contentType(MediaType.APPLICATION_OCTET_STREAM);
         webTestClient.post()
             .uri("/api/transactions/import/999")
             .contentType(MediaType.MULTIPART_FORM_DATA)
-            .bodyValue(bodyBuilder.build())
+            .bodyValue(BodyInserters.fromMultipartData(bodyBuilder.build()))
             .exchange()
             .expectStatus()
             .isEqualTo(202)
